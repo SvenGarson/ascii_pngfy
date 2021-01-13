c87be7ad922333dacca533f81a9245f23e8f650c
Sven Garson <garson_sven@hotmail.com>
2021-01-13 14:05:13 +0100

    Migrate Renderer#set_font_height test suite to Pngfyer
    Add Pngfyer#set_font_height tests suite by migrating and adjusting the Renderer#set_font_height test suite to the Pngfyer class.

--- lib/ascii_pngfy/settings.rb
+++ lib/ascii_pngfy/settings.rb
@@ -4,25 +4,93 @@ module AsciiPngfy
   # Namespace that that contains Setting(s) related functionality
   module Settings
     # Reponsibilities
-    #   - Provides #set argument signature when setting, this setting
-    #   - Keeps track of color as setting
+    #   - Keeps track of the color setting(s)
     #   - Validated through ColorRGBA implicitly
     class ColorSetting
-      def initialize(red, green, blue, alpha)
-        @color = ColorRGBA.new(red, green, blue, alpha)
+      def initialize(initial_red, initial_green, initial_blue, initial_alpha)
+        self.color = ColorRGBA.new(initial_red, initial_green, initial_blue, initial_alpha)
       end
 
       def get
-        @color.dup
+        color.dup
       end
 
       def set(red: nil, green: nil, blue: nil, alpha: nil)
-        @color.red = red unless red.nil?
-        @color.green = green unless green.nil?
-        @color.blue = blue unless blue.nil?
-        @color.alpha = alpha unless alpha.nil?
+        color.red = red unless red.nil?
+        color.green = green unless green.nil?
+        color.blue = blue unless blue.nil?
+        color.alpha = alpha unless alpha.nil?
 
-        @color.dup
+        color.dup
+      end
+
+      private
+
+      attr_accessor(:color)
+    end
+
+    # Reponsibilities
+    #   - Keeps track of the the font_height setting
+    #   - Validates font_height
+    class FontHeightSetting
+      def initialize(initial_font_height)
+        self.font_height = initial_font_height
+      end
+
+      def get
+        font_height
+      end
+
+      def set(desired_font_height)
+        validated_font_height = validate_font_height(desired_font_height)
+
+        new_font_height =
+          if multiple_of_9?(validated_font_height)
+            validated_font_height
+          else
+            lower_bound_distance = (validated_font_height % 9)
+            determine_bound_font_height(validated_font_height, lower_bound_distance)
+          end
+
+        self.font_height = new_font_height
+      end
+
+      private
+
+      attr_accessor(:font_height)
+
+      def font_height_valid?(some_font_height)
+        some_font_height.is_a?(Integer) && (some_font_height >= 9)
+      end
+
+      def validate_font_height(some_font_height)
+        return some_font_height if font_height_valid?(some_font_height)
+
+        error_message = String.new
+        error_message << "#{some_font_height} is not a valid font size. "
+        error_message << 'Must be an Integer in the range (9..).'
+
+        raise AsciiPngfy::Exceptions::InvalidFontHeightError, error_message
+      end
+
+      def multiple_of_9?(number)
+        (number % 9).zero?
+      end
+
+      def lower_bound_distance?(distance)
+        [1, 2, 3, 4].include?(distance)
+      end
+
+      def higher_bound_distance?(distance)
+        [5, 6, 7, 8].include?(distance)
+      end
+
+      def determine_bound_font_height(validated_font_height, lower_bound_distance)
+        if lower_bound_distance?(lower_bound_distance)
+          (validated_font_height / 9) * 9
+        elsif higher_bound_distance?(lower_bound_distance)
+          ((validated_font_height / 9) + 1) * 9
+        end
       end
     end
 
@@ -43,7 +111,8 @@ module AsciiPngfy
         # use builder somehow
         self.settings = {
           font_color: ColorSetting.new(255, 255, 255, 255),
-          background_color: ColorSetting.new(255, 255, 255, 255)
+          background_color: ColorSetting.new(255, 255, 255, 255),
+          font_height: FontHeightSetting.new(9)
         }
       end
 
