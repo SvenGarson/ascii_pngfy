e10f8809562fadba7dcd26c39443d8b3c16388c1
Sven Garson <garson_sven@hotmail.com>
2021-01-29 13:53:18 +0100

    Add more tests for AsciiPngfy::Glyphs

--- test/test_glyphs.rb
+++ test/test_glyphs.rb
@@ -73,4 +73,67 @@ class TestGlyphs < Minitest::Test
       assert(glyph_design_string_contains_only_allowed_design_characters, error_message)
     end
   end
+
+  def test_that_glyphs_design_strings_are_immutable_for_every_supported_non_control_character
+    supported_ascii_characters_without_newline.each do |supported_ascii_character|
+      error_message = "#{AsciiPngfy::Glyphs}::DESIGNS glyph design string for the ASCII character "\
+                      "#{supported_ascii_character.inspect} with ASCII code #{supported_ascii_character.ord} must "\
+                      'be immutable'
+
+      glyph_design_string = AsciiPngfy::Glyphs::DESIGNS[supported_ascii_character]
+
+      assert_raises(FrozenError, error_message) do
+        glyph_design_string.replace('#' * 45)
+      end
+    end
+  end
+
+  def test_that_glyphs_designs_cannot_have_new_character_to_glyph_design_key_value_pairs_added
+    new_and_unsupported_ascii_character_code = 9
+    new_and_unsupported_ascii_character = new_and_unsupported_ascii_character_code.chr
+    new_character_glyph_design = ('.' * 45).freeze
+
+    assert_raises(FrozenError) do
+      AsciiPngfy::Glyphs::DESIGNS[new_and_unsupported_ascii_character] = new_character_glyph_design
+    end
+  end
+
+  def test_that_glyphs_designs_cannot_have_existing_character_to_glyph_design_key_value_pairs_deleted
+    random_supported_non_control_character = supported_ascii_characters_without_newline.sample
+
+    assert_raises(FrozenError) do
+      AsciiPngfy::Glyphs::DESIGNS.delete(random_supported_non_control_character)
+    end
+  end
+
+  def test_that_glyphs_designs_cannot_have_existing_character_keys_replaced
+    existing_character_key = supported_ascii_characters_without_newline.sample
+
+    assert_raises(FrozenError) do
+      # the following is really a two step operation
+      # first the key/value pair is deleted from the hash and the deleted value is returned
+      #
+      # then, the same key (in terms of the string value) is added back to the hash and associated
+      # with the value that was previously associated to the deleted key
+      #
+      # so this is really a combination of deleting and adding key/value pairs from the hash
+      #
+      # for the usage case of this data structure this is sufficient in terms of testing because
+      # this is a mistake that could potentially occur in the renderer implementation, where the
+      # renderer is supposed to exclusively retrieve the character design strings from the hash
+      # and never mutate the hash in any way.
+      #
+      # so this test case is a representation of previous tests but expressed by idiomatic ruby
+      # and how this would typically be achieved by non-dynamic language features
+      AsciiPngfy::Glyphs::DESIGNS[existing_character_key] = AsciiPngfy::Glyphs::DESIGNS.delete(existing_character_key)
+    end
+  end
+
+  def test_that_glyphs_designs_cannot_have_existing_character_design_values_replaced
+    existing_character_key = supported_ascii_characters_without_newline.sample
+
+    assert_raises(FrozenError) do
+      AsciiPngfy::Glyphs::DESIGNS[existing_character_key] = ('.' * 45).freeze
+    end
+  end
 end
